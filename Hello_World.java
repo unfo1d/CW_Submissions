35498d9b9fb3f45c1c34b61a9489a9c451c8689c
unfo1d <rileymatsu18@gmail.com>
2024-08-14 10:28:52 -1000

    Hello_World

---
+++ Hello_World.java
@@ -0,0 +1,15 @@
+/*
+ * This code is for the very first assignment. It does two simple prints and then I submit it.
+ */
+public class Hello_World {
+
+	public static void main(String[] args) {
+		//Task 1: Print out "Hello World"
+		System.out.print("Hello World!");	
+		
+		//TASK 2: Print out "Hello World" and then print it again on the next line
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+	}
+
+}
