c15447aada0662debbe549a47051a03b122bca8f
unfo1d <rileymatsu18@gmail.com>
2025-02-03 09:32:10 -1000

    Arrays2.java

---
+++ CodingBatSolns/Arrays2.java
@@ -0,0 +1,70 @@
+//rotateLeft3
+public int[] rotateLeft3(int[] nums) {
+  int[] tempAr = new int[nums.length];
+  int temp = nums[0];
+  int temp2 = nums[1];
+  tempAr[1] = nums[2];
+  tempAr[0] = temp2;
+  tempAr[2] = temp;
+  return tempAr;
+}
+
+//reverse3
+public int[] reverse3(int[] nums) {
+  int[] tempAr = new int[nums.length];
+  int temp = nums[2];
+  tempAr[2] = nums[0];
+  tempAr[0] = temp;
+  tempAr[1] = nums[1];
+  return tempAr;
+  
+}
+
+//maxEnd3
+public int[] maxEnd3(int[] nums) {
+  int[] tempAr = new int[nums.length];
+  if (nums[2] > nums[0])
+  {
+    tempAr[1] = nums[2];
+    tempAr[0] = nums[2];
+    tempAr[2] = nums[2];
+  }
+  else
+  {
+    tempAr[1] = nums[0];
+    tempAr[2] = nums[0];
+    tempAr[0] = nums[0];
+  }
+  return tempAr;
+}
+
+//sum2
+public int sum2(int[] nums) {
+  int sum = 0;
+  if (nums.length >= 2)
+  {
+    sum = nums[0] + nums[1];
+  }
+  else if (nums.length == 1)
+  {
+    sum = nums[0];
+  }
+  else
+  {
+    return 0;
+  }
+  return sum;
+}
+
+//middleway
+public int[] middleWay(int[] a, int[] b) {
+  int[] Idislikecodingbat = {a[1], b[1]};
+  return Idislikecodingbat;
+
+}
+
+//makeEnds
+public int[] makeEnds(int[] nums) {
+  int[] ends = {nums[0], nums[nums.length-1]};
+  return ends;
+}
