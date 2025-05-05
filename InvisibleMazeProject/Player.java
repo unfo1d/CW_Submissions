68d8b3b474e1e14408bbd6c4b6501ff59256fe67
unfo1d <rileymatsu18@gmail.com>
2025-05-05 00:05:13 -1000

    Player.java

---
+++ InvisibleMazeProject/Player.java
@@ -0,0 +1,25 @@
+public class Player {
+    private int rPos;
+    private int cPos;
+
+    public Player() {
+        rPos = 0;
+        cPos = 0;
+    }
+
+    public int getRPos() {
+        return rPos;
+    }
+
+    public int getCPos() {
+        return cPos;
+    }
+
+    public void setRPos(int r) {
+        rPos = r;
+    }
+
+    public void setCPos(int c) {
+        cPos = c;
+    }
+}
